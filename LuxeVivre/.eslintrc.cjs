d7502596305c602362160f3d144c4ead19b1795a
Eman Yassin <eman.yassin83@gmail.com>
2023-08-03 09:43:31 +0200

    setup file

---
+++ LuxeVivre/.eslintrc.cjs
@@ -0,0 +1,25 @@
+eslint-disable
+
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
+
+// to disable eslint we can type at the top of the file the following comment line (or any other eslint rule) and it will disable eslint for the entire file like this example:
+/* eslint-disable */  
---
+++ LuxeVivre/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1>LuxeVivre</h1>
+      <div className="card">
+        
+      </div>
+     
+    </>
+  )
+}
+
+export default App
