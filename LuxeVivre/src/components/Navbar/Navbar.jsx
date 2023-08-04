472b1626b4646f267121127906c15dc21d874bab
Eman Yassin <eman.yassin83@gmail.com>
2023-08-04 09:36:02 +0200

    updated the subHeading

--- LuxeVivre/src/components/Navbar/Navbar.jsx
+++ LuxeVivre/src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
   const [toggleMenu, setToggleMenu] = useState(false);
 
-
+// the MdOutlineRestaurantMenu is the icon for the close button we got it from react-icons library by installing it with npm install react-icons --save and then importing it in the Navbar.jsx file, we searched for it from 
   return(
   
   <nav className='app__navbar'>
@@ -35,7 +35,7 @@ const Navbar = () => {
       {toggleMenu && (
         <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
         <MdOutlineRestaurantMenu fontsize = {27} className='overlay__close' onClick={()=> setToggleMenu(false)}/>
-        <ul className='app__navbar-smallscreen-links'>
+        <ul className='app__navbar-smallscreen_links'>
       <li className="p__opensans"><a href="#home">Home</a></li>
       <li className="p__opensans"><a href="#about">About</a></li>
       <li className="p__opensans"><a href="#menu">Menu</a></li>
--- LuxeVivre/src/container/Header/Header.jsx
+++ LuxeVivre/src/container/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import SubHeading from '../../components/SubHeading/SubHeading';
+import {images} from '../../constants';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__wrapper_info'>
+      <SubHeading title='Chase the new flavour ' />
+      <h1 className='app__header-h1'>The Key to fine Dining</h1>
+    </div>
+
+    <div className='app__wrapper_img'>
+
+    </div>
   </div>
 );
 
