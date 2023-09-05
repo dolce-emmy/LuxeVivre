b9a63a4b47d11d281d2e95369391a10d2162ddb1
Eman Yassin <eman.yassin83@gmail.com>
2023-09-05 08:57:23 +0200

    updated the chef's content

--- LuxeVivre/src/container/Chef/Chef.jsx
+++ LuxeVivre/src/container/Chef/Chef.jsx
@@ -12,6 +12,23 @@ const Chef = () => (
     <div className='app__wrapper_info'>
       <SubHeading title="Chef's Word"/>
       <h1 className='headtext__cormorant'>What We Believe In</h1>
+
+          <div className='app__chef-content'>
+            <div className='app__chef-content_quote'>
+
+              <img src={images.quote} alt="quote" />
+              <p className='p__opensans'>"At LuxeVivre, I pour my heart and soul into every dish, transforming fresh ingredients into unforgettable culinary experiences.With each dish, we invite you to savor a symphony of flavors, crafted with passion and precision. Join us in indulging your senses in the world of LuxeVivre."</p>
+
+            </div>
+            <p className='p__opensans'></p>
+          </div>
+
+          <div className='app__chef-sign'>
+            <p>Kevin Luo</p>
+            <p className='p__opensans'>Chef and CoFounder</p>
+            <img src={images.sign} alt="sign" />    
+          </div>
+    
     </div>
   </div>
 );
