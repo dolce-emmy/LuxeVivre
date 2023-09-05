b57d4df6a544b266d3532feeefc867f47aaf6a84
Eman Yassin <eman.yassin83@gmail.com>
2023-09-05 08:36:30 +0200

    updated the chef section

--- LuxeVivre/src/container/Chef/Chef.jsx
+++ LuxeVivre/src/container/Chef/Chef.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import {SubHeading} from '../../components';
+import {images} from '../../constants';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className='app__wrapper__img app__wrapper_img-reverse'>
+      <img src={images.chef} alt='chef' />
+    </div>
+
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chef's Word"/>
+      <h1 className='headtext__cormorant'>What We Believe In</h1>
+    </div>
   </div>
 );
 
