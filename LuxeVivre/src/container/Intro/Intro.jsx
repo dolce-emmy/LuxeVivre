ef26477e5040652657d11a6c600b93a11a03e8dd
Eman Yassin <eman.yassin83@gmail.com>
2023-09-06 10:02:09 +0200

    applied the video

--- LuxeVivre/src/container/Intro/Intro.jsx
+++ LuxeVivre/src/container/Intro/Intro.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
+import { meal } from "../../constants";
+import { useRef } from "react";
 
-import './Intro.css';
+import "./Intro.css";
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const vidRef = useRef(null);
+  return (
+    <div className="app__video">
+      <video
+        src={meal}
+        ref={vidRef}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+    </div>
+  );
+};
 
 export default Intro;
