5eb664ee589550fe740e23a40434971244ca7f12
Eman Yassin <eman.yassin83@gmail.com>
2023-09-06 10:14:11 +0200

    added the function to the video button

--- LuxeVivre/src/container/Intro/Intro.jsx
+++ LuxeVivre/src/container/Intro/Intro.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
 import { meal } from "../../constants";
-import { useRef } from "react";
+import { useState, useRef } from "react";
+
 
 import "./Intro.css";
 
 const Intro = () => {
   const vidRef = useRef(null);
+  [playVideo, setPlayVideo] = useState(false);
+
+  const handleVideo = () => {
+    // here we will toggle the video play and pause
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo)
+    if (playVideo) {
+      vidRef.current.pause();
+      setPlayVideo(false);
+    } else {
+      vidRef.current.play();
+      setPlayVideo(true);
+    }
+
+  };
+
   return (
     <div className="app__video">
       <video
@@ -17,6 +33,14 @@ const Intro = () => {
         controls={false}
         muted
       />
+      <div className="app__video-overlay flex__center">
+        <div 
+        className="app__video-overlay_circle flex__center"
+        onClick={handleVideo}
+        >
+
+        </div>
+      </div>
     </div>
   );
 };
