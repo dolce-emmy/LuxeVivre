31950c9d8043cd071fc4ca14047cf369288c499f
Eman Yassin <eman.yassin83@gmail.com>
2023-09-07 08:40:59 +0200

    passed the props to the laurel

--- LuxeVivre/src/container/Laurels/Laurels.jsx
+++ LuxeVivre/src/container/Laurels/Laurels.jsx
@@ -3,11 +3,28 @@ import {SubHeading} from '../../components';
 import {images, data} from '../../constants';
 import './Laurels.css';
 
+const AwardCard = ({award}) => (
+  <div className='app__laurels_awards-card'>
+    <img src={award.imgUrl} alt='award' />
+
+    <div className='app__laurels_awards-card_content'>
+      <p></p>
+      <p></p>
+    </div>
+
+    
+  </div>
+);
+
 const Laurels = () => (
   <div className='app__bg app__wrapper section__padding' id='awards'>
     <div className='app__wrapper_info'>
       <SubHeading title= "Awards & Recognition" />
       <h1 className='headtext__cormorant'> Our Laurels</h1>
+      <div className='app__laurels_awards'>
+        {data.awards.map((award) => <AwardCard award= {award} key={award.title} />)}
+      </div>
+
     </div>
 
     <div className='app__wrapper_img'>
