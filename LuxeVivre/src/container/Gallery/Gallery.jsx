a2dd73150c971a6bf1c9fc3753295fd0818f32bb
Eman Yassin <eman.yassin83@gmail.com>
2023-09-08 09:54:24 +0200

    fixed the gallery images and reefactored mapping those images

--- LuxeVivre/src/container/Gallery/Gallery.jsx
+++ LuxeVivre/src/container/Gallery/Gallery.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import {SubHeading} from '../../components';
 import {BsInstagram, BsArrowLeftShort, BsArrowRightShort} from 'react-icons/bs';
+import { useRef } from 'react';
 import {images} from '../../constants';
 
 import './Gallery.css';
 
+const galleryImages = [
+  images.gallery01,images.gallery02, images.gallery03, images.gallery04
+];
+
 const Gallery = () => {
 
+  const scrollRef = useRef(null);
+
+  const scroll = (direction) => {
+    if(direction === 'left'){
+      scrollRef.current.scrollLeft -= 300;
+    }else{
+      scrollRef.current.scrollLeft += 300;
+    }
+
+  }
   
  return (
   <div className='app__gallery flex__center'>
@@ -19,6 +34,26 @@ const Gallery = () => {
       </p>
       <button type='button' className='custom__button'>View More</button>
     </div>
+
+    <div className='app__gallery-images'>
+
+      <div className='app__gallery-images_container' ref={scrollRef}>
+     {galleryImages.map((image, index) =>(
+      <div className='app__gallery-images_card flex__center' key={`gallery_image-${index + 1}`}>
+          <img src={image} alt="gallery" />
+          <BsInstagram className='gallery__image-icon' />
+      </div>
+     ))}
+      </div>
+      <div className='app__gallery-images_arrow'>
+
+        <BsArrowLeftShort className='gallery__arrow-icon' onClick={() => scroll('left')} />
+        <BsArrowRightShort className='gallery__arrow-icon' onClick={() => scroll('right')} />
+      </div>
+
+      
+    </div>
+
   </div>
 );
 
