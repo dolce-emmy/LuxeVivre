02b1f0aaf257d45f49fc133cbc8b4d665c987f7a
Eman Yassin <eman.yassin83@gmail.com>
2023-09-08 09:13:28 +0200

    updated the gallery section

--- LuxeVivre/src/container/Gallery/Gallery.jsx
+++ LuxeVivre/src/container/Gallery/Gallery.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import {SubHeading} from '../../components';
+import {BsInstagram, BsArrowLeftShort, BsArrowRightShort} from 'react-icons/bs';
+import {images} from '../../constants';
 
 import './Gallery.css';
 
-const Gallery = () => (
-  <div>
-    Gallery
+const Gallery = () => {
+
+  
+ return (
+  <div className='app__gallery flex__center'>
+
+    <div className='app__gallery-content'>
+      <SubHeading title= "Instagram" />
+      <h1 className='headtext__cormorant'>Photo Gallery</h1>
+      <p className='p__opensans' style={{color: '#AAA', marginTop: '2rem'}}>
+      "Explore our Gallery—a visual journey through our culinary artistry and warm ambiance. Get a taste of what makes us special."
+      </p>
+      <button type='button' className='custom__button'>View More</button>
+    </div>
   </div>
 );
+
+}
 
 export default Gallery;
